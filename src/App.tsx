18d7c0f67535428fab590068c928f583d421972e
OliverDoan <doanvietthuan1005@gmail.com>
2023-04-08 16:14:09 +0700

    setup route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import Hello from '~/components/Hello';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import { HomePage } from './pages/HomePage';
+import ErrorPage from './pages/NotFound';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: 'home',
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
 function App() {
-  return <Hello />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
